6a95c663d404012baf25f05b520f8b2c18f1c0b9
Mark Ghian Francis <zetsuya6167@gmail.com>
2026-04-22 16:37:08 +0800

    feat(location): update middleware roles for super-admin route

--- app/Features/Location/Routes/api.php
+++ app/Features/Location/Routes/api.php
@@ -3,10 +3,9 @@
 use App\Features\Location\Controllers\SuperAdmin\LocationController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth:sanctum', 'role:super_admin'])
+Route::middleware(['auth:sanctum', 'role:super_admin,admin'])
     ->prefix('super-admin')
     ->group(function () {
-        // lightweight list for select inputs (id + name)
         Route::get('locations/list', [LocationController::class, 'listSimple']);
         Route::apiResource('locations', LocationController::class);
     });
